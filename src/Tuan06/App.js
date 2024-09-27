7fdb1920b138fcb0bb1aadac3da5bbf82d089ecc
HKThanh <21086351.thanh@student.iuh.edu.vn>
2024-09-27 08:52:55 +0700

    ReactNative week6 Custom header Navigation

--- src/Tuan06/App.js
+++ src/Tuan06/App.js
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OneColumn from './Screens/OneColumn';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="OneColumn" component={OneColumn} options={{
+          headerTitle: "Chat",
+          headerTitleStyle: {
+            fontSize: 24,
+            color: "#fff"
+          },
+          headerTitleAlign: "center",
+          headerRight: () => (
+            <Button
+              onPress={() => alert('This is a button!')}
+              title="Info"
+              color="#fff"
+            />
+          ),
+          headerLeft: () => (
+            <Button
+              onPress={() => alert('This is a button!')}
+              title="Back"
+              color="#fff"
+            />
+          ),
+          headerStyle: {
+            backgroundColor: "#1BA9FF",
+          }
+        }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
